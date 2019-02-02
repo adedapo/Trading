d56d08e13bc543f9b371d5bff68375f6f33921f8
alabiaa <MaMais50!>
2019-02-02 16:39:44 -0500

    Sample.py experiements with data retrueval using the yahoo finance API

---
+++ Sample.py
@@ -0,0 +1,24 @@
+import sys
+from yahoo_finance_api2 import share
+from yahoo_finance_api2.exceptions import YahooFinanceError
+from datetime import datetime
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+my_share = share.Share('MSFT')
+symbol_data = None
+
+try:
+    symbol_data = my_share.get_historical(share.PERIOD_TYPE_DAY,
+                                          10,
+                                          share.FREQUENCY_TYPE_MINUTE,
+                                          5)
+
+except YahooFinanceError as e:
+    print(e.message)
+    sys.exit(1)
+
+timestamp = [datetime.utcfromtimestamp(i/1000) for i in symbol_data['timestamp']]
+trace = go.Candlestick(x=timestamp, open=symbol_data['open'], high=symbol_data['high'], low=symbol_data['low'], close=symbol_data['close'])
+data = [trace]
+py.plot(data)
